fa0b0bb7fcdaa749c41c5bb5bb06fd25c0cd3275
jktchikapa <jktchikapa@gmail.com>
2025-12-30 17:25:37 -0500

    Initial commit : create the project

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace fantastic_zoo_console_game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+"# fantastic-zoo-console-game" 
